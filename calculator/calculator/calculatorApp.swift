d26f635c9a44772d221a7015caa48c09958d513e
Mar1naMakarenko <153446656+Mar1naMakarenko@users.noreply.github.com>
2024-05-30 03:36:01 +0300

    Add files via upload
    the project file

---
+++ calculator/calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Марина Макаренко on 01.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CalculatorScreen()
+        }
+    }
+}
